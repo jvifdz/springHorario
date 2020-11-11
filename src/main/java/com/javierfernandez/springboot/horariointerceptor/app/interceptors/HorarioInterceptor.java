a72429100ca7c0a291b037d8bddc5cdb3ba00f2e
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-11-11 13:22:54 +0100

    Creando el interceptor del Horario

---
+++ src/main/java/com/javierfernandez/springboot/horariointerceptor/app/interceptors/HorarioInterceptor.java
@@ -0,0 +1,53 @@
+package com.javierfernandez.springboot.horariointerceptor.app.interceptors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+
+@Component
+public class HorarioInterceptor implements HandlerInterceptor {
+
+    //Value para inyectar cosas del properties
+    @Value("${config.horario.apertura}")
+    private Integer apertura;
+    @Value("${config.horario.cierre}")
+    private Integer cierre;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        int hora = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (hora>=apertura&& hora<cierre){
+
+            StringBuilder mensaje= new StringBuilder("Bienvenido al horario de atencion a clientes");
+            //El append es para concatenar con el StringBuilder
+            mensaje.append(", atendemos desde las ");
+            mensaje.append(apertura);
+            mensaje.append("hrs. ");
+            mensaje.append("hasta las: ");
+            mensaje.append(cierre);
+            mensaje.append("hrs. ");
+            mensaje.append("Gracias por su visita.");
+            request.setAttribute("mensaje",mensaje.toString());
+            return true;
+        }
+
+        response.sendRedirect(request.getContextPath().concat("/cerrado"));
+
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        String mensaje = (String) request.getAttribute("mensaje");
+        modelAndView.addObject("horario", mensaje);
+
+    }
+}
