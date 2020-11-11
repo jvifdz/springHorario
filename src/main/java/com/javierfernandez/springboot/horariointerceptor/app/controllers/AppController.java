66e8e74bdbbbb083f3ff1be667c0aeb386a21784
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-11-11 13:28:29 +0100

    Creando la vista cerrado y su getMapping en el controlador

--- src/main/java/com/javierfernandez/springboot/horariointerceptor/app/controllers/AppController.java
+++ src/main/java/com/javierfernandez/springboot/horariointerceptor/app/controllers/AppController.java
@@ -1,5 +1,6 @@
 package com.javierfernandez.springboot.horariointerceptor.app.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 
+    //Value para inyectar cosas del properties
+    @Value("${config.horario.apertura}")
+    private Integer apertura;
+    @Value("${config.horario.cierre}")
+    private Integer cierre;
+
+
     @GetMapping({"/","/index"})
     public String index(Model model){
         model.addAttribute("titulo","Bienvendio al horario de atención a clientes");
         return "index";
+    }
+    @GetMapping("/cerrado")
+    public String cerrrado(Model model){
+        //apend para concatenar en el String builder
+        StringBuilder mensaje = new StringBuilder("Cerrado, por favor visítenos desde las ");
+        mensaje.append(apertura);
+        mensaje.append(" y las ");
+        mensaje.append(cierre);
+        mensaje.append(" hrs. Gracias ");
+        model.addAttribute("titulo","Fuera del horario de atención");
+        model.addAttribute("mensaje", mensaje);
+        return "cerrado";
+
     }
 }
